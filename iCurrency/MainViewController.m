c58a7e59f3528245550edebb878eb0934cc025f5
lwt0023 <258008977@qq.com>
2016-03-01 16:37:52 +0800

    add the edit mode of cell
    finish delete and move row function of cells

--- iCurrency/MainViewController.m
+++ iCurrency/MainViewController.m
@@ -14,8 +14,14 @@
 
 @implementation MainViewController
 {
-    NSArray *items;//用于存储国家名称
+    NSMutableArray *nameitems;//用于存储国家名称
+    NSMutableArray *currencyUnit;//用于存储货币单位
     NSArray *images;//用于存储图片
+    
+}
+- (IBAction)editMode:(id)sender {
+    
+        
 }
 
 
@@ -23,12 +29,11 @@
     [super viewDidLoad];
     
     //可以在这里设置左右导航按钮
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    items = [@"USA CNY JPY" componentsSeparatedByString:@" "];
-    
-    images = [@"usa china jp"componentsSeparatedByString:@" "];
-    
-    
+    nameitems = [@"USA CNY JPY UK" componentsSeparatedByString:@" "];
+    currencyUnit = [@"美元 人民币 日元 英镑"componentsSeparatedByString:@" "];
+    images = [@"usa china jp uk"componentsSeparatedByString:@" "];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -48,7 +53,8 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 //    该区段的行数
 #warning Incomplete implementation, return the number of rows
-    return items.count;
+    NSLog(@"此时一共加载了%d个国家",nameitems.count);
+    return nameitems.count;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -62,25 +68,147 @@
     
     ConvertCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
     
-    cell.countryName.text = [items objectAtIndex:indexPath.row];
+    cell.countryName.text = [nameitems objectAtIndex:indexPath.row];
     cell.countryImage.image = [UIImage imageNamed:[images objectAtIndex:indexPath.row]];
+    cell.currencyUnit.text = [currencyUnit objectAtIndex:indexPath.row];
     
+    
+    
+    //    新建一个数据模型的类，由数据模型类提供表格里的内容再import到本类里面  在cell.countryName=..这样引入数据
 
     
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"点击了一下第 %ld 个cell",indexPath.row+1);
+    //创建一个弹出窗口
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"温馨提示" message:@"你选择了一个国家" delegate:self cancelButtonTitle:@"暂时不用" otherButtonTitles:@"确定", nil];
+//    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alert show];
 
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tab forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
 }
-*/
+
+
+// - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+// if (editingStyle == UITableViewCellEditingStyleDelete) {
+// 
+// [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+// } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+// 
+// }
+// }
+
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+//当进入编辑模式下时，左导航按钮变为添加
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSLog(@"删除");
+        [nameitems removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+//    else if (items.count == 0){
+//        NSLog(@"重新加载所有数据");
+//        [tableView reloadData];
+//    }
+    else if (editingStyle == UITableViewCellEditingStyleInsert)
+    {
+        NSLog(@"添加模式");
+//        NSArray *indexPaths = @[indexPath];
+//        [tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:(UITableViewRowAnimationRight)];
+
+        // 添加一条数据
+        [nameitems insertObject:[NSString stringWithFormat:@"%@", @"增加的Cell"] atIndex:indexPath.row + 1];
+        
+        NSIndexPath *path = [NSIndexPath indexPathForRow:indexPath.row + 1 inSection:0];
+        // 注意点:数组中插入的条数必须和tableview界面上插入的cell条一致
+        // 否则程序会报错
+        [self.tableView insertRowsAtIndexPaths:@[path] withRowAnimation:UITableViewRowAnimationBottom];
+
+    }
+    
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    //移动行
+    NSLog(@"进入移动行模式");
+    NSString *fromObj = [nameitems objectAtIndex:sourceIndexPath.row];
+    [nameitems insertObject:fromObj atIndex:destinationIndexPath.row];
+    [nameitems removeObjectAtIndex:sourceIndexPath.row];
+    
+    
+}
+
+//- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+//{
+//    //移动
+//    NSString* fromObj = [self._arrayName objectAtIndex:sourceIndexPath.row];
+//    [self._arrayName insertObject:fromObj atIndex:destinationIndexPath.row];
+//    [self._arrayName removeObjectAtIndex:sourceIndexPath.row];
+//}
+
+
+// 用于告诉系统开启的编辑模式是什么模式
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    if (indexPath.row % 2 == 0) {
+//        // 偶数，添加
+//        return UITableViewCellEditingStyleInsert;
+//    } else {
+//        // 奇数，删除
+//        return UITableViewCellEditingStyleDelete;
+//    }
+    
+    return UITableViewCellEditingStyleDelete;
+}
+
+
+-(NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    本函数用于修改 删除按钮显示的信息
+    return @"删除";
+}
+
+
+-(void)setEditing:(BOOL)editing animated:(BOOL)animated
+
+{
+    
+    [super setEditing:editing animated:animated];
+    
+    [self.tableView setEditing:editing animated:animated];
+    
+}
+
+
+
+
+
+
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    判断是否可以移动行
+    return YES;
+}
+
+
+
+
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+//    NSLog(@"可以编辑行");
+    return YES;
+}
+
+
+
+
+
+
+
 
 /*
 // Override to support conditional editing of the table view.
@@ -91,13 +219,13 @@
 */
 
 /*
-// Override to support editing the table view.
+ Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+         Delete the row from the data source
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+         Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
 */
